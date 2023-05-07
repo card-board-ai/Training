31313afafc25a7beae2412d254c691031647bbc8
Josh Phillips <drag0n2ooth230@gmail.com>
2023-05-06 22:24:44 -0600

    api key config refactoring and start on Cards trainer

--- CardsTrainer.py
+++ CardsTrainer.py
@@ -1,4 +1,5 @@
 import os
+import configparser
 from supabase.client import Client, create_client
 
 local_supabase_url = "http://localhost:54321"
@@ -6,23 +7,8 @@ local_supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZ
 prod_supabase_url = "https://tzrsrwethpgxfeukzgkw.supabase.co"
 prod_supabase_private_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6cnNyd2V0aHBneGZldWt6Z2t3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MjEzNzcwOSwiZXhwIjoxOTk3NzEzNzA5fQ.MjUipkQNje2SIFGDZTjyicFjsfH1GME2PPlkrV8FdqI"
 supabase: Client = create_client(prod_supabase_url, prod_supabase_private_key)
-openai = "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"
+config = configparser.ConfigParser()
+config.read('keys.cfg')
 
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import SupabaseVectorStore
-from langchain.document_loaders import TextLoader
-
-loader = TextLoader("../MagicRulesApril14.txt")
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=800, chunk_overlap=50)
-docs = text_splitter.split_documents(documents)
-
-embeddings = OpenAIEmbeddings(openai_api_key=openai)
-print("embeddings created")
-
-SupabaseVectorStore.from_documents(
-    docs, embeddings, client=supabase
-)
-
-print("Done")
