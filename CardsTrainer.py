4d2108344ffe27c3eb115330ce1808b6b20c84c7
Josh Phillips <drag0n2ooth230@gmail.com>
2023-05-06 23:45:05 -0600

    updated/alter requirements refactored some api keys start work on card CardsTrainer

--- CardsTrainer.py
+++ CardsTrainer.py
@@ -1,14 +1,27 @@
 import os
 import configparser
 from supabase.client import Client, create_client
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import SupabaseVectorStore
+from langchain.document_loaders import JSONLoader
+import json
+from pathlib import Path
+from pprint import pprint
 
-local_supabase_url = "http://localhost:54321"
-local_supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
-prod_supabase_url = "https://tzrsrwethpgxfeukzgkw.supabase.co"
-prod_supabase_private_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6cnNyd2V0aHBneGZldWt6Z2t3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MjEzNzcwOSwiZXhwIjoxOTk3NzEzNzA5fQ.MjUipkQNje2SIFGDZTjyicFjsfH1GME2PPlkrV8FdqI"
-supabase: Client = create_client(prod_supabase_url, prod_supabase_private_key)
 config = configparser.ConfigParser()
 config.read('keys.cfg')
 
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import SupabaseVectorStore
+environment = input("Which environment are we training, local or prod?")
+
+if environment == "local":
+    supabase: Client = create_client(config.get('Supabase', 'local_url'), config.get('Supabase', 'local_key'))
+elif environment == "prod":
+    supabase: Client = create_client(config.get('Supabase', 'prod_url'), config.get('Supabase', 'prod_private_key'))
+else:
+    raise Exception("That is not an available environment")
+
+loader = JSONLoader(
+    file_path='../default-cards-20230506211056.json',
+    jq_schema='.messages[].content')
+
+data = loader.load()
--- RulesTrainer.py
+++ RulesTrainer.py
@@ -1,6 +1,10 @@
 import os
 import configparser
 from supabase.client import Client, create_client
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import SupabaseVectorStore
+from langchain.document_loaders import TextLoader
 
 local_supabase_url = "http://localhost:54321"
 local_supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
@@ -9,11 +13,6 @@ prod_supabase_private_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdX
 supabase: Client = create_client(prod_supabase_url, prod_supabase_private_key)
 config = configparser.ConfigParser()
 config.read('keys.cfg')
-
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import SupabaseVectorStore
-from langchain.document_loaders import TextLoader
 
 loader = TextLoader("../MagicRulesApril14.txt")
 documents = loader.load()
