59de9d66677c32f147642358d5805fa11bdfe188
j1philli <jphilli1996@gmail.com>
2023-09-13 01:04:53 -0600

    move supabase training to external comm

--- external_comm.py
+++ external_comm.py
@@ -1,7 +1,24 @@
 import requests
+from langchain.vectorstores import SupabaseVectorStore
+from langchain.embeddings.openai import OpenAIEmbeddings
+import configparser
 
-def downloader(website, file_name, file_format):
+config = configparser.ConfigParser()
+config.read('keys.cfg')
+
+embeddings = OpenAIEmbeddings(openai_api_key=config.get('OpenAI', 'key'))
+
+def web_downloader(website, file_name, file_format):
     query_parameters = {"downloadformat": f"{file_format}"}
     response = requests.get(website, params=query_parameters)
     print(f"{file_name} done fetching")
     return response.json()
+
+def supa_trainer(table, documents, supa_client):
+    #TODO add the database wipe here
+    SupabaseVectorStore.from_documents(documents, embeddings, client=supa_client,
+                                       table_name=table, show_progress=True)
+    
+def supa_compare(supa_client):
+    return
+
