1828046387c9ff9425b9b3deb07c2bc052ae0e77
j1philli <jphilli1996@gmail.com>
2023-09-19 13:15:16 -0400

    add db wipe to supaTrainer

--- external_comm.py
+++ external_comm.py
@@ -12,10 +12,23 @@ def web_downloader(website, file_name, file_format):
     query_parameters = {"downloadformat": f"{file_format}"}
     response = requests.get(website, params=query_parameters)
     print(f"{file_name} done fetching")
-    return response.json()
+    if file_format == "json":
+        return response.json()
+    elif file_format == "txt":
+        return response.content
+    else:
+        raise Exception("web_downloader can not handle that file type")
 
-def supa_trainer(table, documents, supa_client):
-    #TODO add the database wipe here
+def supa_trainer(table, documents, supa_client, supa_auth, file, file_format):
+    headers = {"Content-Type": "application/json", "Authorization": str(supa_auth)}
+    data = {
+    "table": table
+    }
+    dbwipe_response = requests.post(config.get('Supabase', 'table_wipe_url'), 
+                             headers=headers, json=data)
+    print("vectorDbWipe server respons =" + dbwipe_response)
+    up_response = supa_client.storage.from_('training documents').upload(f"/{table}/{table}.{file_format}", file)
+    print("uploading to supabase respons=" + up_response)
     SupabaseVectorStore.from_documents(documents, embeddings, client=supa_client,
                                        table_name=table, show_progress=True)
     
