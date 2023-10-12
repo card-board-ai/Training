ca57cd8a87756fb3f8e36b82ceb91a307ec3f523
j1philli <jphilli1996@gmail.com>
2023-10-12 00:32:41 -0600

    take out rich for now

--- lorcana.py
+++ lorcana.py
@@ -1,9 +1,10 @@
-import rich
+
 
 def lorcana_rules_loader():
 
-    rich.print("hi from lorcana rules loader")
+    print("hi from lorcana rules loader")
+
 
 def lorcana_cards_loader():
 
-    rich.print("hi from lorcana cards loader")
+    print("hi from lorcana cards loader")
