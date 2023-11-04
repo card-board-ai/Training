b359b9798a256fa1d921d68d0714375d6bb8bc66
j1philli <jphilli1996@gmail.com>
2023-11-04 00:00:00 -0600

    seperate train logic from main and add a second option to create a new game

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from supabase.client import Client, create_client
 from simple_term_menu import TerminalMenu
 from rich.console import Console
-from inspect import isfunction
 import configparser
-import importlib
-import inspect
+from train import traindbs
+from new_game import create_game
 
 console = Console()
 
@@ -27,56 +26,13 @@ elif menu_options[env_choice] == "Prod":
 else:
     raise Exception("That is not an available environment")
 
+games = supa_client.table('games').select("*").execute()
 
-# grab the file names from the db
-games = supa_client.table('games').select("training_file").execute()
-menu_options = ["Select All"]
-functions = []
-
-
-# this iterates through the provided fil names from db and creates lists for the menu and function execution
-for item in games.data:
-    file = item.get('training_file')
-    module = importlib.import_module(file)
-    # based on https://stackoverflow.com/questions/66084762/call-function-from-another-file-without-import-clause
-    for attribute_name in dir(module):
-        attribute = getattr(module, attribute_name)
-        if isfunction(attribute) and not attribute_name.startswith("_"):
-            menu_options.append(attribute.__name__)
-            functions.append(attribute)
-
-# display cli menu
-terminal_menu = TerminalMenu(
-    menu_options,
-    title='',
-    multi_select=True,
-    show_multi_select_hint=True,)
-choice_index = terminal_menu.show()
-
-
-def build_args(function):
-    signature = inspect.signature(function)
-    parameters = signature.parameters
-    func_args = {}
-    for param in parameters:
-        if param == "supa_client":
-            func_args.update({param: supa_client})
-        elif param == "supa_key":
-            func_args.update({param: supa_key})
-        elif param == "config":
-            func_args.update({param: config})
-    return func_args
-
-
-"""
-if the choice includes 'Select All' then execute all
-available functions otherwise iterate through fireing the chossen functions
-"""
-if choice_index[0] == 0:
-    for item in functions:
-        args = build_args(item)
-        item(**args)
-else:
-    for item in choice_index:
-        args = build_args(functions[item - 1])
-        functions[item - 1](**args)
+action_options = ["Create new game", "Train"]
+action_menu = TerminalMenu(action_options)
+action_index = action_menu.show()
+match action_index:
+    case 0:  # creat new game
+        create_game(games, supa_client)
+    case 1:  # train
+        traindbs(games, config, supa_key, supa_client)
